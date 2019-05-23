7845e2397807babed7248bc141c4ab22eaf9e156
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-23 12:45:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 function nestedTarget() {
-  return document.getElementById('nested').querySelector('target');
+  return document.getElementById('nested').querySelector('.target');
 }
